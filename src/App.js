3a9ee349e45c3cc36e9a354cb35864ada5f067f9
nimishgo <nimish.2711@gmail.com>
2022-11-07 11:59:39 +0530

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
