20a1c0c80e4dfc7ecfd6f7b3ebe5bb8766342e60
nimishgo <nimish.2711@gmail.com>
2022-11-12 10:56:02 +0530

    fix : hompage svg image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../styles/home.scss";
 import wavebottom from "../assets/wave-bottom.svg";
+import Button from "../components/Button";
 const Home = () => {
   return (
     <>
       <div className="container">
+        <Button />
         <div className="blob"></div>
       </div>
       <img src={wavebottom} alt="wavebottom" className="waveBottom" />
