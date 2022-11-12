ad15b3d9bf846f582b6602f6499fac8211350b8a
nimishgo <nimish.2711@gmail.com>
2022-11-12 10:53:56 +0530

    fix : fixed z-index

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import { FiShoppingCart } from "react-icons/fi";
 import "../styles/header.scss";
 
-const Header = () => {
-  const [count] = useState(1);
+const Header = ({ cart }) => {
   return (
     <div className="header">
       <Link to="/">
@@ -22,12 +21,12 @@ const Header = () => {
             <Link to="/Contact">Contacts</Link>
           </li>
           <li>
-            <Link>
+            <Link to="/Carts">
               <div className="base">
                 <FiShoppingCart />
                 <div className="indicator">
-                  <div className="noti_count" role="status" value={count}>
-                    {count}
+                  <div className="noti_count" role="status" value={cart.size}>
+                    {cart.size}
                   </div>
                 </div>
               </div>
